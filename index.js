2efb2fee5a8a2308d19e4d4506b72ccb46991531
4padma <tulasipadma333@gmail.com>
2020-09-03 11:11:48 +0530

    added dependncised

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const http = require('http');
+const express= require('express');
+const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
+const { text } = require('express');
+
+const app = express();
+
+// sgMail.setApiKey('IZOrI8WaS0GF8jaLCupF8A');
+
+const sendgrid_username = 'santhosh';
+    const sendgrid_password = 'Password@123';
+    //var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+    // const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+
+app.use(cors());
+
+//welcome page
+
+// app.get('/',(req,res) => {
+//     res.send("welcome to GFT");
+// })
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
