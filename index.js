08e3b0545494b6e35d94ccd602ce664b326df944
4padma <tulasipadma333@gmail.com>
2020-09-03 11:28:58 +0530

    test

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ app.use(cors());
 
 //welcome page
 
-// app.get('/',(req,res) => {
-//     res.send("welcome to GFT");
-// })
+app.get('/',(req,res) => {
+    res.send("welcome to GFT");
+})
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("am again testing World!");
-});
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("am again testing World!");
+// });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// const port = process.env.PORT || 1337;
+// server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
